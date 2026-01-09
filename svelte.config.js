4c33d66384002f428d356c6f40ee3cdb6a838a75
Natcel <natcelnieves@gmail.com>
2026-01-09 17:31:07 -0400

    update

--- svelte.config.js
+++
@@ -1,6 +0,0 @@
-import adapter from '@sveltejs/adapter-static';
-
-/** @type {import('@sveltejs/kit').Config} */
-const config = { kit: { adapter: adapter(), paths: { base: process.env.NODE_ENV === 'production' ? '/natcel0711.github.io' : '' } } };
-
-export default config;
